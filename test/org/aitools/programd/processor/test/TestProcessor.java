fefdee1af602a256068341a2385d748d82649871
mario.scriminaci <mario.scriminaci@75138c76-dfce-11dd-9b7c-bbba4c9295f9>
2009-07-16 19:29:14 +0000

    Init plugin configuration

---
+++ test/org/aitools/programd/processor/test/TestProcessor.java
@@ -0,0 +1,33 @@
+package org.aitools.programd.processor.test;
+
+import org.aitools.programd.Core;
+import org.aitools.programd.parser.TemplateParser;
+import org.aitools.programd.processor.ProcessorException;
+import org.aitools.programd.processor.aiml.AIMLProcessor;
+import org.aitools.programd.server.core.test.TestSupport;
+import org.w3c.dom.Element;
+
+public class TestProcessor extends AIMLProcessor {
+
+	public static String label = "test";
+	
+	
+	public TestProcessor(Core coreToUse) {
+		super(coreToUse);
+	}
+	
+	@Override
+	public String process(Element element, TemplateParser parser)
+			throws ProcessorException {
+		if (element.getChildNodes() != null) {
+			
+			TestSupport support = (TestSupport)Core.getPluginSupportMap().get("TestSupport");
+			
+			return element.getTextContent()+"; TEST! "+support.getCiao();			
+		} else {
+			throw new ProcessorException(
+					"<test></test> must have content!", null);
+		}
+	}
+
+}
